3f19240f109d44f5b19a9e7fe42973fcb79328f9
Miëtek Bak <mietek@bak.io>
2014-11-02 17:09:25 +0000

    Automatically install or update Halcyon

--- src.sh
+++ src.sh
@@ -1,19 +1,32 @@
 export BUILDPACK_TOP_DIR
 BUILDPACK_TOP_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )
 
-if [[ ! -d "${BUILDPACK_TOP_DIR}/lib/halcyon" ]]; then
-	if ! git clone --depth=1 --quiet 'https://github.com/mietek/halcyon.git' "${BUILDPACK_TOP_DIR}/lib/halcyon"; then
-		echo '   *** ERROR: Cannot clone Halcyon' >&2
-		exit 1
+install_halcyon () {
+	local dir
+	dir="${BUILDPACK_TOP_DIR}/lib/halcyon"
+	if [[ -d "${dir}" ]]; then
+		if ! git -C "${dir}" fetch -q || ! git -C "${dir}" reset -q --hard '@{u}'; then
+			echo '   *** ERROR: Cannot update Halcyon' >&2
+			return 1
+		fi
+		return 0
 	fi
-fi
 
-if [[ ! -d "${BUILDPACK_TOP_DIR}/lib/halcyon/lib/bashmenot" ]]; then
-	if ! git clone --depth=1 --quiet 'https://github.com/mietek/bashmenot.git' "${BUILDPACK_TOP_DIR}/lib/halcyon/lib/bashmenot"; then
-		echo '   *** ERROR: Cannot clone bashmenot' >&2
-		exit 1
+	local urloid url branch
+	urloid="${HALCYON_SOURCE_URL:-https://github.com/mietek/halcyon.git}"
+	url="${urloid%#*}"
+	branch="${urloid#*#}"
+	if ! git clone -q "${url}" "${dir}"; then
+		echo "   *** ERROR: Cannot clone ${url}" >&2
+		return 1
 	fi
-fi
+	if [[ "${url}" != "${branch}" ]] && ! git -C "${dir}" checkout -q "${branch}"; then
+		echo "   *** ERROR: Cannot checkout Halcyon ${branch}" >&2
+		return 1
+	fi
+}
+
+install_halcyon || exit 1
 
 source "${BUILDPACK_TOP_DIR}/lib/halcyon/src.sh"
 
