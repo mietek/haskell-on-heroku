ebf1d44e5707e785d65735bdfbd524f21683da1a
Miëtek Bak <mietek@bak.io>
2014-11-03 03:31:56 +0000

    Simplify use of PATH

--- profile.d/buildpack.sh
+++ profile.d/buildpack.sh
@@ -1,9 +1,6 @@
 if ! (( ${BUILDPACK_INTERNAL_PATHS:-0} )); then
 	export BUILDPACK_INTERNAL_PATHS=1
 
-	export PATH="/app/.buildpack/bin:${PATH}"
+	export PATH="/app/.buildpack/bin:/app/.buildpack/lib/halcyon:${PATH}"
 	export PATH="/app/.halcyon/slug/bin:${PATH}"
 fi
-
-source '/app/.buildpack/lib/halcyon/src/paths.sh'
-set_halcyon_paths
