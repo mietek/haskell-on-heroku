010681bdfb143bdedde390dc28e90ebd148943ec
Miëtek Bak <mietek@bak.io>
2014-11-04 03:11:53 +0000

    Update use of Halcyon

--- profile.d/buildpack.sh
+++ profile.d/buildpack.sh
@@ -1,7 +1,7 @@
 if ! (( ${BUILDPACK_INTERNAL_PATHS:-0} )); then
 	export BUILDPACK_INTERNAL_PATHS=1
 
-	export PATH="/app/.buildpack/bin:/app/.buildpack/lib/halcyon:${PATH:-}"
+	export PATH="/app/.buildpack/bin:${PATH:-}"
 
 	HALCYON_NO_AUTOUPDATE=1 source <( halcyon show-paths )
 fi
