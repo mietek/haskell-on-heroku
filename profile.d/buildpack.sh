a5bc14c0904694f7bf36a04fe9778cd43b65c7e3
Miëtek Bak <mietek@bak.io>
2014-12-01 13:27:53 +0000

    Improve profile.d script

--- profile.d/buildpack.sh
+++ profile.d/buildpack.sh
@@ -1,20 +1,36 @@
+export BUILDPACK_DIR='/app/.buildpack'
+
+
+# NOTE: Self-updating is disabled on worker dynos.
+
 if [[ "${DYNO%.*}" != 'run' ]]; then
 	export BUILDPACK_NO_SELF_UPDATE=1
 	export HALCYON_NO_SELF_UPDATE=1
 	export BASHMENOT_NO_SELF_UPDATE=1
 fi
 
-if ! (( ${BUILDPACK_INTERNAL_PATHS:-0} )); then
-	export BUILDPACK_INTERNAL_PATHS=1
-
-	export PATH="/app/.buildpack/bin:${PATH:-}"
-
-	source <( HALCYON_NO_SELF_UPDATE=1 /app/.buildpack/lib/halcyon/halcyon paths )
-fi
 
 if [[ -n "${BUILDPACK_SSH_PRIVATE_KEY}" ]]; then
 	mkdir -p '/app/.ssh'
+
 	echo "${BUILDPACK_SSH_PRIVATE_KEY}" >'/app/.ssh/id_rsa'
-	echo -e 'Host *\n  StrictHostKeyChecking no\n  UserKnownHostsFile=/dev/null' >'/app/.ssh/config'
 	unset BUILDPACK_SSH_PRIVATE_KEY
+
+	chmod 700 '/app/.ssh'
+	chmod 600 '/app/.ssh/id_rsa'
+
+	cat <<-EOF >'/app/.ssh/config'
+		Host *
+		  StrictHostKeyChecking no
+		  UserKnownHostsFile=/dev/null
+EOF
+fi
+
+
+if ! (( ${BUILDPACK_INTERNAL_PATHS:-0} )); then
+	export BUILDPACK_INTERNAL_PATHS=1
+
+	export PATH="${BUILDPACK_DIR}/bin:${PATH:-}"
+
+	source <( HALCYON_NO_SELF_UPDATE=1 "${BUILDPACK_DIR}/lib/halcyon/halcyon" paths )
 fi
